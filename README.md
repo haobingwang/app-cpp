070003ca93d4541ba8fee43af2ef7b4b9cf86b49
haobing <hobart.cn@gmail.com>
2018-12-01 17:15:01 +0800

    frist run

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # app-cpp
+
 cpp 学习使用
+
+运行在 Windows 环境下
--- myfirst.cpp
+++ myfirst.cpp
@@ -1,9 +1,14 @@
+using namespace std;
+
 #include <iostream>
 int main()
 {
-    using namespace std;
     cout << "Come up and C++ me some time.";
     cout << endl;
-    count << "You won't regret it!" << endl;
+    cout << "You won't regret it!" << endl;
+
+    // 为了让程序不闪退
+    cin.get();
+
     return 0;
 }
